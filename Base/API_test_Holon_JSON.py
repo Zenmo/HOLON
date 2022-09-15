bc2064a27768f1ca149df6a0efc89f260df83e40
GillisHommen <98752265+GillisHommen@users.noreply.github.com>
2022-09-15 10:36:27 +0200

    Update private cloud, rekentijd model
    * Private cloud kan worden aangeroepen met python scripts
    * 'hashmaps' met energyflows die elke tijdstap gecleared en gevuld worden uitgezet tbv rekentijd
    * Wat minder belangrijke aanpassingen in diverse excells en jsons gebruikt om te experimenteren met private cloud

--- Base/API_test_Holon_JSON.py
+++ Base/API_test_Holon_JSON.py
@@ -4,106 +4,115 @@ import time
 
 from anylogiccloudclient.client.cloud_client import CloudClient
 
+
 class AnyLogicExperiment:
-    client = CloudClient("f105b75c-4265-4c79-ab36-a9d6e7532fc0")
-    version = client.get_latest_model_version("Holon JSON")
-    inputs = ''
-    simulation = ''
-    outputs = ''
+    # client = CloudClient("f105b75c-4265-4c79-ab36-a9d6e7532fc0")
+
+    client = CloudClient("23cd7f85-d254-406b-b69c-66d23dfc9fd8", "http://5.9.100.203")
+    version = client.get_latest_model_version("Base")
+    inputs = ""
+    simulation = ""
+    outputs = ""
     duration_s = 0
 
     def __init__(self, experiment):
         self.experiment = experiment
-        #self.model = model
-    
-    
+        # self.model = model
+
     def setInputs(self):
-        #actorInputFile1 = 'config_actors_moreAgents.txt'
-        #gridConnectionInputFile1 = 'config_netConnections_moreAgents.txt'
-    
+        # actorInputFile1 = 'config_actors_moreAgents.txt'
+        # gridConnectionInputFile1 = 'config_netConnections_moreAgents.txt'
+
         # Create an Inputs object with the default input values
         self.inputs = self.client.create_default_inputs(self.version)
         print(self.inputs.names())
 
-        #print(path+actorInputFile1)
+        # print(path+actorInputFile1)
 
-        #with open(self.experiment.path+"\\"+actorInputFile1,'r') as f:
+        # with open(self.experiment.path+"\\"+actorInputFile1,'r') as f:
         #    ActorInputdata1 = json.loads(f.read())
 
-        #with open(self.experiment.path+"\\"+gridConnectionInputFile1,'r') as f:
+        # with open(self.experiment.path+"\\"+gridConnectionInputFile1,'r') as f:
         #    gridConnectionInputdata1 = json.loads(f.read())
 
-        #print("**** actordata from file ***")
-        #print(ActorInputdata1)
-        #print("**** actordata from experiment object ****")
-        #print(self.experiment.actorsConfigData)
-        #self.experiment.actorsConfigData = json.loads(self.experiment.actorsConfigData)
-        #self.experiment.gridConnectionConfigData = json.loads()
-        #print("*** reimaginged ***")
-        #print(actordata)
-        #gridConnectionInputdata1
+        # print("**** actordata from file ***")
+        # print(ActorInputdata1)
+        # print("**** actordata from experiment object ****")
+        # print(self.experiment.actorsConfigData)
+        # self.experiment.actorsConfigData = json.loads(self.experiment.actorsConfigData)
+        # self.experiment.gridConnectionConfigData = json.loads()
+        # print("*** reimaginged ***")
+        # print(actordata)
+        # gridConnectionInputdata1
 
-        self.inputs.set_input('P actors config JSON', self.experiment.actorsConfigData )
-        self.inputs.set_input('P grid connection config JSON', self.experiment.gridConnectionConfigData)
-        self.inputs.set_input('P grid node config JSON', self.experiment.gridNodeConfigData) 
-        self.inputs.set_input('P energy assets config JSON', self.experiment.energyAssetConfigData)
-        self.inputs.set_input('P time step h', self.experiment.timeStep_h)
+        self.inputs.set_input("P actors config JSON", self.experiment.actorsConfigData)
+        self.inputs.set_input(
+            "P grid connection config JSON", self.experiment.gridConnectionConfigData
+        )
+        self.inputs.set_input(
+            "P grid node config JSON", self.experiment.gridNodeConfigData
+        )
+        self.inputs.set_input(
+            "P energy assets config JSON", self.experiment.energyAssetConfigData
+        )
+        self.inputs.set_input("P time step h", self.experiment.timeStep_h)
 
         print("inputs set!")
-        
+
     def runSimulation(self):
         startTime_ms = round(time.time() * 1000)
         print("working...")
         self.simulation = self.client.create_simulation(self.inputs)
 
         self.outputs = self.simulation.get_outputs_and_run_if_absent()
-        print("Outputs: ",self.outputs.names())
-        jsonOutput = self.outputs.value('O output JSON')
-        exceptions = self.outputs.value('O output exceptions')
+        print("Outputs: ", self.outputs.names())
+        jsonOutput = self.outputs.value("O output JSON")
+        exceptions = self.outputs.value("O output exceptions")
         print("Returned exceptions: ", exceptions)
         endTime_ms = round(time.time() * 1000)
-        
-        self.duration_s = (endTime_ms - startTime_ms ) / 1000
-        print("cloud response time = ", self.duration_s ," s")
+
+        self.duration_s = (endTime_ms - startTime_ms) / 1000
+        print("cloud response time = ", self.duration_s, " s")
 
         data = json.loads(jsonOutput)
-        df_nested_list = pd.json_normalize(data, record_path =['greatSucces'])
+        df_nested_list = pd.json_normalize(data, record_path=["greatSucces"])
         print(df_nested_list)
-        df_nested_list.to_excel(self.experiment.path+"\\"+"outputAPI_"+self.experiment.name+".xlsx")
+        df_nested_list.to_excel(
+            self.experiment.path + "\\" + "outputAPI_" + self.experiment.name + ".xlsx"
+        )
 
-    
     #
     ##### Experiment 2 with other json inputs and fetch outputs
 
-    #actorInputFile2 = 'config_actors2.txt'
-    #gridConnectionInputFile2 = 'config_netConnections2.txt'
+    # actorInputFile2 = 'config_actors2.txt'
+    # gridConnectionInputFile2 = 'config_netConnections2.txt'
 
-    #print(path+actorInputFile2)
+    # print(path+actorInputFile2)
 
-    #with open(path+"\\"+actorInputFile2,'r') as f:
+    # with open(path+"\\"+actorInputFile2,'r') as f:
     #    ActorInputdata2 = json.loads(f.read())
 
-    #with open(path+"\\"+gridConnectionInputFile2,'r') as f:
+    # with open(path+"\\"+gridConnectionInputFile2,'r') as f:
     #    gridConnectionInputdata2 = json.loads(f.read())
 
-    #ActorInputdata2
-    #gridConnectionInputdata2
+    # ActorInputdata2
+    # gridConnectionInputdata2
 
-    #inputs2 = client.create_default_inputs(version)
-    #inputs2.names()
+    # inputs2 = client.create_default_inputs(version)
+    # inputs2.names()
 
-    #inputs2.set_input('P actors config JSON', ActorInputdata2)
-    #inputs2.set_input('P grid connection config JSON', gridConnectionInputdata2)
+    # inputs2.set_input('P actors config JSON', ActorInputdata2)
+    # inputs2.set_input('P grid connection config JSON', gridConnectionInputdata2)
 
-    #simulation2 = client.create_simulation(inputs2)
-    #outputs2 = simulation2.get_outputs_and_run_if_absent()
-    #outputs2.names()
-    #jsonOutput2 = outputs2.value('O output JSON')
-    #errors2 = outputs2.value('O output exceptions')
-    #print(errors2)
+    # simulation2 = client.create_simulation(inputs2)
+    # outputs2 = simulation2.get_outputs_and_run_if_absent()
+    # outputs2.names()
+    # jsonOutput2 = outputs2.value('O output JSON')
+    # errors2 = outputs2.value('O output exceptions')
+    # print(errors2)
 
-    #data2 = json.loads(jsonOutput2)
-    #print(data2)
-    #df_nested_list2 = pd.json_normalize(data2, record_path =['greatSucces'])
-    #print(df_nested_list2)
-    #df_nested_list2.to_excel(path+"\\"+"outputAPI2.xlsx")   
+    # data2 = json.loads(jsonOutput2)
+    # print(data2)
+    # df_nested_list2 = pd.json_normalize(data2, record_path =['greatSucces'])
+    # print(df_nested_list2)
+    # df_nested_list2.to_excel(path+"\\"+"outputAPI2.xlsx")
--- Base/defineExperiments.py
+++ Base/defineExperiments.py
@@ -4,25 +4,26 @@ import API_test_Holon_JSON as api
 
 path = str(pathlib.Path(__file__).parent.resolve())
 
-#Experiment default
-experiment_default = exp.Experiment(path, "default", "db_backboneConfig.xlsx", 1)
-experiment_default.printSettings()
-experiment_default.generateConfigJSONs()
-experiment_default.actorsConfigData
+# Experiment default
+# experiment_default = exp.Experiment(path, "default", "db_backboneConfig.xlsx", 1)
+# experiment_default.printSettings()
+# experiment_default.generateConfigJSONs()
+# experiment_default.actorsConfigData
 
-# Run experiment in AnyLogic Cloud
-api_experiment_default = api.AnyLogicExperiment(experiment_default)
-api_experiment_default.setInputs()
-api_experiment_default.runSimulation()
-print("duration: ", api_experiment_default.duration_s, " seconds")
+# # Run experiment in AnyLogic Cloud
+# api_experiment_default = api.AnyLogicExperiment(experiment_default)
+# api_experiment_default.setInputs()
+# api_experiment_default.runSimulation()
+# print("duration: ", api_experiment_default.duration_s, " seconds")
 
 ##
-#Experiment with 300 agents
-experiment_300 = exp.Experiment(path, "300_gridConnections", "db_backboneConfig_300.xlsx", 1)
+# Experiment with 300 agents
+experiment_300 = exp.Experiment(
+    path, "300_gridConnections", "db_backboneConfig_300.xlsx", 1
+)
 experiment_300.printSettings()
 experiment_300.generateConfigJSONs()
 experiment_300.actorsConfigData
-experiment_default.actorsConfigData
 
 # Run experiment in AnyLogic Cloud
 api_experiment_300 = api.AnyLogicExperiment(experiment_300)
@@ -31,20 +32,16 @@ api_experiment_300.runSimulation()
 print("duration: ", api_experiment_300.duration_s, " seconds")
 
 
-
-#Experiment with 1000 agents
-experiment_1000 = exp.Experiment(path, "1000_gridConnections", "db_backboneConfig_1000.xlsx", 1)
+# Experiment with 1000 agents
+experiment_1000 = exp.Experiment(
+    path, "1000_gridConnections", "db_backboneConfig_1000.xlsx", 1
+)
 experiment_1000.printSettings()
 experiment_1000.generateConfigJSONs()
 experiment_1000.actorsConfigData
-experiment_default.actorsConfigData
 
 # Run experiment in AnyLogic Cloud
 api_experiment_1000 = api.AnyLogicExperiment(experiment_1000)
 api_experiment_1000.setInputs()
 api_experiment_1000.runSimulation()
 print("duration: ", api_experiment_1000.duration_s, " seconds")
-
-
-
-
