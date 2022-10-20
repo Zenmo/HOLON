e0ccb85a2b0dbf5abf8e596eb5190c0b02274dca
thesethtruth <s.w.p.vanwieringen@gmail.com>
2022-10-20 13:47:28 +0200

    adds api key to config test

--- run_cloud_experiments/tests/test_config.py
+++ run_cloud_experiments/tests/test_config.py
@@ -3,3 +3,4 @@ from experiments.config import config
 def test_config():
     assert config['anylogic_cloud']
     assert config['anylogic_cloud']['url']
+    assert config['anylogic_cloud']['api_key']
