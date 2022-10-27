8f1d82a605900a4c4aace9301cf735215e81fab7
thesethtruth <s.w.p.vanwieringen@gmail.com>
2022-10-27 14:52:27 +0200

    adds todo to polling period

--- run_cloud_experiments/experiments/client/__init__.py
+++ run_cloud_experiments/experiments/client/__init__.py
@@ -2,18 +2,17 @@ from anylogiccloudclient.client.cloud_client import CloudClient
 from experiments.config import config
 from .inputs import Inputs
 
+
 class Client(Inputs):
     def __init__(self, experiment):
         self.client = CloudClient(
-            config['anylogic_cloud']['api_key'],  config['anylogic_cloud']['url']
+            config["anylogic_cloud"]["api_key"], config["anylogic_cloud"]["url"]
         )
         self.experiment = experiment
         self.inputs = experiment.model_name
 
     def run_simulation(self, polling_period=10):
-        '''Returns the runs outputs'''
-        return self.client.create_simulation(
-                self.inputs
-            ).get_outputs_and_run_if_absent(polling_period=polling_period)
-
-
+        """Returns the runs outputs TODO: polling period is hardcoded"""
+        return self.client.create_simulation(self.inputs).get_outputs_and_run_if_absent(
+            polling_period=polling_period
+        )
