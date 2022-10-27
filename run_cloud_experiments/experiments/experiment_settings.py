567b4b91ab406763cd7f50fdb22be047aae56468
GillisHommen <98752265+GillisHommen@users.noreply.github.com>
2022-10-27 10:53:50 +0200

    Modified experiment 300 grid connections to use BaseParTest model in cloud

--- run_cloud_experiments/experiments/experiment_settings.py
+++ run_cloud_experiments/experiments/experiment_settings.py
@@ -3,36 +3,36 @@ from pathlib import Path
 
 TOP_FOLDER = Path(__file__).parent
 
-class ExperimentSettings():
 
+class ExperimentSettings:
     def __init__(self, experiments):
-       self.experiments = experiments
+        self.experiments = experiments
 
     def all(self):
-        '''Returns a Generator of experiments'''
+        """Returns a Generator of experiments"""
         return (self.get(name, settings) for name, settings in self.experiments.items())
 
     def get(self, name, settings):
         return ExperimentSetting(name, settings).setting
 
     def find(self, experiment_name):
-        '''
+        """
         Returns settings for the expermient with that name,
         return blank settings if it doesn't exist
-        '''
+        """
         self.get(experiment_name, self.experiments.get(experiment_name, {}))
 
     @classmethod
-    def load(cls, path='config/experiments.yml'):
+    def load(cls, path="config/experiments.yml"):
         path = TOP_FOLDER.parent / path
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             doc = yaml.load(f, Loader=yaml.FullLoader)
         return cls(doc)
 
 
-class ExperimentSetting():
+class ExperimentSetting:
     def __init__(self, name, settings):
-        settings['name'] = name
+        settings["name"] = name
         self.setting = settings
 
     @property
@@ -42,4 +42,4 @@ class ExperimentSetting():
     @setting.setter
     def setting(self, settings):
         self._setting = settings
-        self._setting['path'] = (TOP_FOLDER / settings.get('path')).resolve()
+        self._setting["path"] = (TOP_FOLDER / settings.get("path")).resolve()
