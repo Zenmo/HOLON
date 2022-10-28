c7383c3bbee9bd890f9de2afac972597eec3317f
thesethtruth <s.w.p.vanwieringen@gmail.com>
2022-10-28 22:14:55 +0200

    replaces the folder by a submodule with shallow depth

--- run_cloud_experiments/tests/test_config.py
+++
@@ -1,6 +0,0 @@
-from experiments.config import config
-
-def test_config():
-    assert config['anylogic_cloud']
-    assert config['anylogic_cloud']['url']
-    assert config['anylogic_cloud']['api_key']
