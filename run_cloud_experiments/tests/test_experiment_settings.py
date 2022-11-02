94e5a97a2d08761e0d74c2d5694ccc2bd3f706e1
JorritvandenHouten <98388036+JorritvandenHouten@users.noreply.github.com>
2022-11-02 14:43:19 +0100

    Feature total cost output data (#148)
    * Absolute piekbelasting per netvlak berekend en uitgevoerd
    
    *Per gridnode wordt de absolute piekbelasting, gemiddelde belasting en load factor (efficient infra-gebruik) bijgehouden voor berekening van totale kosten buiten het model en validatie van sturingsmechanimen en slim gedrag.
    *Aan het einde van de modelrun worden per netvlak de piekbelastingen opgeteld, gecombineerd in een dedicated json-file en uitgestuurd als output van het model.
    * Pretty (formatted) print voor output-json-files voor zowel in de cloud als lokaal op de schijf.
    
    * Hourly total system electricity import, export KPI
    
    *Import en export in kWh over HSMS-node(s) worden per uur bijgehouden in tijdreeksen om externe verrekening met uurlijkse e-prijs(prijzen) en visualisaties mogelijk te maken.
    *Tijdreeksen worden uitgevoerd als onderdeel van de totalCostKPI-output-json.
    *Checksums toegevoegd voor controle van totalen over de modelrun (wijkt wat af van "totalElectricityImported_MWh" en "totalElectricityExported_MWh" in OutputRunSettingsData.json; mogelijk door afrondingsfouten.
    
    * Overall molecular energy imports and exports, industry case, KPI-Visuals agent fixed
    
    *Totale import en export van methaan en waterstof in het model worden berekend en uitgestuurd als onderdeel van de TotalCostKPI-data aan het einde van de modelrun
    *Modelconfiguratie gemaakt voor industrie-case.
    *Electric Heavy Goods vehicle toegevoegd in defaultEnergyAssets.xslx
    *KPI-Visuals-agent werkt weer, industry-gridconnection-categorie toegevoegd.
    
    * DistrictBattery type added, EnergyAssets config by defaultEnergyAssets
    
    *DistrictBattery gridConnection type has been added to the model
    *DistrictBattery defaultEnergyAsset has been added to the defaultEnergyAssets excel
    *Configuration of EnergyAssets in the model now works with references to defaultEnergyAssets, to make it possible to add different types of assets from the config-excel easily and leanly.
    *KPI-visuals agent has been augmented to include DistrictBattery.
    *Total electricity import and export tijdreeksen: fixed double counting error for new timestep.
    
    * Fixes for merge with MVP-branche
    
    Fixes for merge with MVPcase-branche
    
    * GridBattery holon balancing strategy, batteryCharging KPI's
    
    *Working flexible GridBattery operating strategy, balancing holon energy surplus or deficit,
    *Added basic batteryCharging KPI's to determine number of cycles for cost calculations outside the model.
    *Fixed battery capacity error resulting from hidden hardcoded value...
    
    * Charging cycle calculation KPI's per Battery asset type
    
    *KPI data for total battery charging, decharging and installed storage capacity are now calculated per battery type and reported separately at the end of the model run.
    
    * EV's as electric storage, opgenomen in battery KPIs
    
    *EV's as electric storage
    *EV's opgenomen in battery total Cost KPIs
    *Aantal units per type toegevoegd aan Battery total cost KPI's
    
    * Adds submodule
    
    * Removed old experiments module files

--- run_cloud_experiments/tests/test_experiment_settings.py
+++
@@ -1,9 +0,0 @@
-from experiments.experiment_settings import ExperimentSettings
-
-def test_setting():
-    settings = ExperimentSettings.load()
-
-    for setting in settings.all():
-        assert 'show_progress' in setting
-        assert 'name' in setting
-        assert setting['timestep_hours'] > 0
