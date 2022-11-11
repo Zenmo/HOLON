dc88b36ee0dad566ba2acf7d70fa54f8c5b276ae
JorritvandenHouten <jorrit.van.den.houten@witteveenbos.com>
2022-11-11 13:22:06 +0100

    Modeluitkomsten cloud opslaan naar lokale json-files
    *Aanpassingen (nog in de preprocessing-versie van de module) waarmee modeluitkomsten uit de cloud opgeslagen kunnen worden als lokale json-files. Maakt debugging en verifieren van uitkomsten bij grote data-sets makkelijker/mogelijk.

--- preprocessing/experiments/anylogic_experiment/outcomes.py
+++ preprocessing/experiments/anylogic_experiment/outcomes.py
@@ -15,15 +15,32 @@ class Outcomes():
 
     def write_outcomes(self):
         for outcome in self.experiment.outcomes:
-            if outcome['write']:
-                self._write_outcome(outcome)
+            if outcome['writeExcel']:
+                self._write_outcomeExcel(outcome)
+            if outcome['writeJSON']:
+                self._write_outcomeJSON(outcome)
             if outcome['print']:
                 print(self.outcomes[outcome['human_key']])
 
-    def _write_outcome(self, outcome):
+    def _write_outcomeExcel(self, outcome):
         self.outcomes[outcome['human_key']].to_excel(
             self._output_folder() / f"{outcome['human_key']}_{self.experiment.name}.xlsx"
         )
+
+    def _write_outcomeJSON(self, outcome, formatted: bool = True):
+        json_output = self.outcomes[outcome['human_key']]
+        #json_output = self.outcomes[outcome['human_key']].to_json()
+        print("json_output:")
+        print(json_output)
+
+        with open(self._output_folder() / f"{outcome['human_key']}_{self.experiment.name}.json", "w") as outfile:
+            if formatted:
+                outfile.write(json.dumps(json.loads(self.outcomes[outcome['human_key']].to_json(orient="records")), indent=2))
+                #print("_write jsons exported")
+            else:
+                outfile.writelines(json.loads([self.outcomes[outcome['human_key']]].to_json(
+            orient="records"
+        )))
 
     def _output_folder(self):
         path = self.experiment.path / 'output' / self.experiment.name
@@ -32,14 +49,18 @@ class Outcomes():
 
     def _calc_value(self, outputs, outcome):
         raw_data = json.loads(outputs.value(outcome['anylogic_key']))
-
+        print("checking calc value rawdata:")
+        print(raw_data)
         # Some special cases to make json_normalize accept the object
-        if isinstance(raw_data, list) and isinstance(raw_data[0], float):
-            raw_data =  [{outcome['human_key']: raw_value} for raw_value in raw_data]
-        elif isinstance(raw_data, float):
-            raw_data = {outcome['human_key']: raw_data}
+        #if isinstance(raw_data, list) and isinstance(raw_data[0], float):
+        #    raw_data =  [{outcome['human_key']: raw_value} for raw_value in raw_data]
+        #elif isinstance(raw_data, float):
+        #    raw_data = {outcome['human_key']: raw_data}
+
+        raw_data = {outcome['human_key']: raw_data}
 
         value = pd.json_normalize(raw_data)
+        #value = raw_data
 
         if outcome.get('action', '') == 'normalise':
             return value / value.sum()
